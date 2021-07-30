190248fac4255c355f7f284173f1128e57d8a31a
linshengxi <linshengxi@bingsn.com>
2021-07-30 10:55:09 +0800

    并发学习项目初始化，导入jdk8 源码（可编辑）

---
+++ src/main/java/cn/lsx/concurrent/longadder/Demo.java
@@ -0,0 +1,14 @@
+package cn.lsx.concurrent.longadder;
+
+import java.util.concurrent.atomic.LongAdder;
+
+/**
+ * @author linShengxi
+ * @date 2021/7/30
+ */
+
+public class Demo {
+    public static void main(String[] args) {
+        LongAdder longAdder = new LongAdder();
+    }
+}
